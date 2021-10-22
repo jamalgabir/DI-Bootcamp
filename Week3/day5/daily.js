9b4633979533225c288a81edffdc264703ca03e2
jamalgabir <jamalgamy@gmail.com>
2021-10-22 09:52:47 +0300

    I make Some New things in my dailly challenge Donegit statusgit status!

--- Week3/day5/daily.js
+++ Week3/day5/daily.js
@@ -12,10 +12,25 @@ for (let i = 0;i<7;i++){
 for(let i = 0; i<7*7;i++){
 	let newdivr = document.createElement("div")
 	newdivr.style.background = "black";
+	newdivr.style.color = "red";
+	let txt = document.createTextNode("*")
+	
+    if (i>1&&i<5){
+    	newdivr.appendChild(txt)
+    }else if(i==8||i==12||i==15||i==19){
+    	newdivr.appendChild(txt)
+    }else if(i>21&&i<27){
+    	newdivr.appendChild(txt)
+    }else if(i==29||i==33||i==36||i==40||i==43||i==47){
+    	newdivr.appendChild(txt)
+    }
+
+	console.log(newdivr.length)
 	right.appendChild(newdivr)
-	//console.log(newdivr.length)
+	
+
 }
-console.log(right);
+//console.log(right);
 
 for (let i = 0;i<7;i++){
 	let ne = document.createElement("div");
