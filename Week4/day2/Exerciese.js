4e0f3145fb09fc48ab835ee5e1d79aae1c629ca2
jamalgabir <jamalgamy@gmail.com>
2021-10-25 22:40:23 +0300

    Exercises XP Gold SOLVED DONE

--- Week4/day2/Exerciese.js
+++ Week4/day2/Exerciese.js
@@ -31,43 +31,75 @@ console.log(func());
 //------------------------------------------
 // Create a one line function that 
 // receives two numbers as parameters and returns the sum.
-const Exercies1 = (a,b) => a+b;
-console.log(Exercies1(4,6));
+// const Exercies1 = (a,b) => a+b;
+// console.log(Exercies1(4,6));
 //--------------------
 //Exercise 2 : Closure
 //--------------------
 // Analyse the code below, 
 // and before executing it, predict the outcome of the last line.
 //IT WILL BE 13
-const addTo = x => y => x + y;
-var addToTen = addTo(10);
-addToTen(3);
+// const addTo = x => y => x + y;
+// var addToTen = addTo(10);
+// addToTen(3);
 //OUTPUT = 13;
 //---------------------
 //Exercise 3 : Currying
 //---------------------
 // Analyse the code below, 
 // and before executing it, predict the outcome of the last line.
-const sum = (a, b) => a + b
-const curriedSum = (a) => (b) => a + b
-curriedSum(30)(1);
-//OUTPUT = 31;
+// const sum1 = (a, b) => a + b
+// const curriedSum = (a) => (b) => a + b
+// curriedSum(30)(1);
+// //OUTPUT = 31;
 //----------------------------
 //Exercise 4 : Currying
 //---------------------------
-const sum = (a, b) => a + b
-const curriedSum = (a) => (b) => a + b
-const add5 = curriedSum(5)
-add5(12)
+// const sum = (a, b) => a + b
+// const curriedSum = (a) => (b) => a + b
+// const add5 = curriedSum(5)
+// add5(12)
 //OUTPUT = 17;
 //---------------------------------
 //Exercise 5 : Composing
 //-------------------------------
-const compose = (f, g) => (a) => f(g(a));
-const add1 = (num) => num + 1;
-const add5 = (num) => num + 5;
-compose(add1, add5)(10);
+// const compose = (f, g) => (a) => f(g(a));
+// const add1 = (num) => num + 1;
+// const add5 = (num) => num + 5;
+// compose(add1, add5)(10);
 ////OUTPUT = 16;
 //------------------------------
 //GREAT EXERCIESE
-//----------------------
+//--------------------------
+
+//---------------------------
+//Exercises XP Gold
+//-------------------------
+
+let landscape = function()
+{
+ let result = "";
+ let flat = function(x)
+ {
+   for(let count = 0; count<x; count++)
+   {
+     result = result + "_";
+   }
+ }
+ let mountain = function(x)
+ {
+   result = result + "/"
+   for(let counter = 0; counter<x; counter++)
+   {
+     result = result + "'"
+   }
+   result = result + "\\"
+ }
+
+flat(4);
+mountain(4);
+flat(4)
+return result;
+}
+
+console.log(landscape());
