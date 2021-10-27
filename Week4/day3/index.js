0146b3ad6333e9dd1b8fd79ddeb437cf8ef8841e
jamalgabir <jamalgamy@gmail.com>
2021-10-27 10:41:01 +0300

    Daily Challenge

--- Week4/day3/index.js
+++ Week4/day3/index.js
@@ -42,28 +42,37 @@ const studentsFootball = [
 // { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
 
 // let newusers = users.map(element => (` Hello ${element.firstName}`));
-// //console.log(newusers);
-
-// let users2 = users.filter(word => (word.role==="Full Stack Resident"));
-// console.log(users2);
-// let fs = [];
-// let fullstack = users2.filter(name => (`Congratulations ${name.firstName}`));
-// console.log(fullstack.firstName);
-
-// let newArray = [2, 3, 4, 5, 6]; 
-
-// // newArray.forEach(function(number, i) { 
-// //     newArray[i] = newArray[i]*2; 
-// // }); 
-
-// // let newArray = [2, 3, 4, 5, 6]; 
-
-// // newArray.forEach(function(number, i) { 
-// //     newArray[i] = number * 2; 
-// // }); 
-// console.log(newArray);
-
-// newArray.forEach((element, index) => {
-//     console.log(`Current index: ${index}`);
-//     console.log(element);
-// });
+let inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 }, /// push to new array
+  {
+    id: 4,
+    car_make: "Land Rover",
+    car_model: "Defender Ice Edition",
+    car_year: 2010,
+  },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+let getCarHonda = (carInventory) => {
+  let hondaArray = inventory.filter((v) => v.car_make === "Honda");
+  let firstHonda = hondaArray[0];
+  console.log(hondaArray, firstHonda);
+  return `This is a ${firstHonda.car_make} ${firstHonda.car_model} from ${firstHonda.car_year}`;
+};
+console.log(getCarHonda(inventory));
+//10:01
+let inventory2 = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  {
+    id: 4,
+    car_make: "Land Rover",
+    car_model: "Defender Ice Edition",
+    car_year: 2010,
+  },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+const sortCarInventoryByYear = (carInventory) =>carInventory.sort((a, b) => (a.car_year > b.car_year && 1) || -1);
+console.log(sortCarInventoryByYear(inventory2));
