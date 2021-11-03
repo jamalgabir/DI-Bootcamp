3397d48bdb5868276bdfbc54f77c7eb4c5132e82
jamalgabir <jamalgamy@gmail.com>
2021-11-03 22:06:08 +0200

    NEW

---
+++ Week5/day3/Exercies.js
@@ -0,0 +1,21 @@
+console.log('it work');
+function compareToTen(num){
+	return new Promise((resolve,reject)=>{
+		if(num>10){
+			resolve(`${num} greater than 10, seccess !`)
+		}
+		else if(num<10){
+			resolve(`${num} less than 10, error !`)
+	    }else{
+	    	reject('noooooo')
+	    }
+
+	})
+};
+compareToTen(43)
+.then(re => console.log(re))
+.catch(erroe => console.log(erroe));
+
+compareToTen(8)
+.then(re => console.log(re))
+.catch(erroe => console.log(erroe));
