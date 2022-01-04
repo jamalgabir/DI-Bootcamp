7b798e3c68b1accbb2650b36ce7a8a27c6d724e9
jamalgabir <jamalgamy@gmail.com>
2022-01-04 11:34:43 +0200

    new push

--- Week10/day1/Exercies/myapp/src/Component/MemeGenerator.js
+++ Week10/day1/Exercies/myapp/src/Component/MemeGenerator.js
@@ -1,26 +1,47 @@
 import React,{Component} from "react";
 
 class MemeGenerator extends Component{
+    constructor(){
+        super()
+        this.state={
+            data:[],
+            url:''
+            
+        }
+    }
     
     
     
     
     
-    
-    
+    componentDidMount(){
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res=>res.json())
+        .then(data=>{(this.setState({data:data}))})
+        
+    }
     render(){
+         const info = this.state.data
+        // let img = info.map((item,i)=>{
+        //     return this.setState({url:item[0].url})
+        // })
+        //console.log(url)
         let rl='http://i.imgflip.com/1bij.jpg';
         
         return(
             <>
-            <input type='text' name='toptext'/> <input type='text' name='btntext'/> 
+            {info.map((img)=>(
+                <div>{JSON.stringify(img)}</div>
+            ))}
+            {/* <input type='text' name='toptext'/> <input type='text' name='btntext'/> 
              <button>Gen</button>
+             
              <div id='img' >
                 <h2 id='top'>top text</h2>
                 <h2 id='bt'>button text</h2>
                 <img src={rl}/>
                 
-             </div>
+             </div> */}
              
             </>
         )
